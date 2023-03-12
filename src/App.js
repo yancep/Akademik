d906bbed3c4ed5ec2c2ad4171145fada5079b8a0
Yan Carlos Escobar Pupo <escobarpupoyancarlos1@gmail.com>
2023-03-12 15:54:12 -0400

    added basis=c estructure of login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 
 import './App.css';
 
+
+import Login from './components/Login.js';
+import Home from './components/Home.js';
+import React, { useState } from "react";
 function App() {
+  const [user,setUser] = useState([])
   return (
     <div className="App">
+      {
+        !user.length>0? <Login setUser={setUser}/>:<Home setUser={setUser}/>
+      }
+      
     </div>
   );
 }
